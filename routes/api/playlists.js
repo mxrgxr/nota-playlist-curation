514475e5c7e22ace56e2090719b630a068cdc8f4
mxrgxr <mgxrcias@gmail.com>
2023-08-26 14:28:43 -0700

    wrap backend routes with checktoken middleware

--- routes/api/playlists.js
+++ routes/api/playlists.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users')
+const checkAccessToken = require('../../config/checkAccessToken')
 
-router.get('/', usersCtrl.getPlaylists);
+router.get('/', checkAccessToken, usersCtrl.getPlaylists);
 
 module.exports = router;
