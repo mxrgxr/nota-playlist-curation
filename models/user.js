3e9edb8e5d35ed0f673eb1b30151d1d6db2f0d33
mxrgxr <mgxrcias@gmail.com>
2023-08-25 22:08:42 -0700

    embed playlist and track schemas within user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,71 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const trackSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  uri: {
+    type: String,
+    required: true,
+  },
+});
+
+const playlistSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  images: [
+    {
+      url: String,
+      width: Number,
+      height: Number,
+    },
+  ],
+  name: {
+    type: String,
+    required: true,
+  },
+  tracks: [trackSchema], // Embed the Track schema
+  uri: {
+    type: String,
+    required: true,
+  },
+  filters: {
+    artists: [String],
+    acousticness: {
+      min: Number,
+      max: Number,
+    },
+    danceability: {
+      min: Number,
+      max: Number,
+    },
+    energy: {
+      min: Number,
+      max: Number,
+    },
+    instrumentalness: {
+      min: Number,
+      max: Number,
+    },
+    tempo: {
+      min: Number,
+      max: Number,
+    },
+    valence: {
+      min: Number,
+      max: Number,
+    },
+  },
+});
+
 const userSchema = new Schema({
     spotifyId: {
       type: String,
@@ -18,6 +83,7 @@ const userSchema = new Schema({
     refreshToken: {
       type: String,
     },
-});
+    playlists: [playlistSchema]
+  });
 
 module.exports = mongoose.model('User', userSchema);
--- src/components/Dashboard/PlaylistGrid.jsx
+++ src/components/Dashboard/PlaylistGrid.jsx
@@ -1,9 +1,15 @@
-import PlaylistCard from "./PlaylistCard"
+import PlaylistCard from './PlaylistCard';
 
-export default function PlaylistGrid(){
-    return (
-        <div>
-          {/* <PlaylistCard imgSrc={imgSrc} playlistName={playlistName} /> */}
-        </div>
-    );
+export default function PlaylistGrid() {
+//   return (
+//     <div>
+//       {playlists.map((playlist) => (
+//         <PlaylistCard
+//           key={playlist.id}
+//           imgSrc={playlist.images[0]?.url}
+//           playlistName={playlist.name}
+//         />
+//       ))}
+//     </div>
+//   );
 }
