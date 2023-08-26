89fd653eef95c5ca38d4df619aabb0f829520aa5
mxrgxr <mgxrcias@gmail.com>
2023-08-26 14:39:01 -0700

    use variable to standardize spotify api reqs

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const BASE_URL = 'https://api.spotify.com/v1'
 module.exports = {
   getPlaylists
 };
@@ -7,14 +7,14 @@ module.exports = {
 async function getPlaylists(req, res) {
     try{
       const accessToken = req.headers.authorization.split(' ')[1];
-      const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
+      const response = await axios.get(BASE_URL + '/me/playlists', {
       headers: {
         'Authorization': `Bearer ${accessToken}`,
       },
     });
     res.json(response.data);
   } catch (error) {
-    console.error('Error fetching playlists in controller:', error);
+    console.error('Error fetching playlists:', error);
     res.status(500).json({ error: 'Error fetching playlists' });
   }
 }
