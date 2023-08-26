c3ccd06852378540c3d75c5ef7c94ba314308c56
mxrgxr <mgxrcias@gmail.com>
2023-08-26 04:04:41 -0700

    configure backend route to handle fetching playlists

--- routes/api/playlists.js
+++ routes/api/playlists.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const usersCtrl = require('../../controllers/api/users')
 
-router.get('/', passport.authenticate('spotify', { session: false }), usersCtrl.getPlaylists);
+router.get('/', usersCtrl.getPlaylists);
 
 module.exports = router;
