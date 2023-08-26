f454fa99d26882e98c6201c2a41320528715fa7a
mxrgxr <mgxrcias@gmail.com>
2023-08-26 04:00:23 -0700

    add refresh token logic to strategy

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,28 @@ const passport = require('passport');
 const SpotifyStrategy = require('passport-spotify').Strategy;
 const User = require('../models/user');
 
+async function refreshAccessToken(refreshToken) {
+  try {
+    const authOptions = {
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        'Authorization': 'Basic ' + (Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+      })
+    };
+
+    const response = await axios(authOptions);
+    return response.data.access_token;
+  } catch (error) {
+    throw error;
+  }
+}
+
 passport.use(
   new SpotifyStrategy(
     {
@@ -14,15 +36,28 @@ passport.use(
       try {
         let user = await User.findOne({ spotifyId: profile.id });
         if (user) {
+          const now = new Date();
+          if (user.tokenExpiration && user.tokenExpiration <= now) {
+            try {
+              const newAccessToken = await refreshAccessToken(refreshToken);
+              user.accessToken = newAccessToken;
+              user.tokenExpiration = new Date(Date.now() + expires_in * 1000);
+              await user.save();
+            } catch (err) {
+              console.error('Error refreshing access token:', err);
+            }
+          }
           return done(null, user);
         }
         const email = profile.emails && profile.emails.length > 0 ? profile.emails[0].value : null;
+        const tokenExpiration = new Date(Date.now() + expires_in * 1000);
         user = await User.create({
           spotifyId: profile.id,
           displayName: profile.displayName,
           email: email,
           accessToken: accessToken,
           refreshToken: refreshToken,
+          tokenExpiration: tokenExpiration,
         });
         return done(null, user);
       } catch (err) {
