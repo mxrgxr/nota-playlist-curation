804c863d6831afaa5b8a23ee258ff8ca0772ffd0
mxrgxr <mgxrcias@gmail.com>
2023-08-26 04:03:49 -0700

    standardize backend api requests

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,11 +1,18 @@
-export default async function sendRequest(url, method = 'GET', payload = null) {
-    const options = { method };
-    if (payload) {
-      options.headers = { 'Content-Type': 'application/json' };
-      options.body = JSON.stringify(payload);
-    }
-    const res = await fetch(url, options);
-    if (res.ok) return res.json();
-    throw new Error('Bad Request');
+import { getToken } from './users-service';
+
+export default async function sendRequest(url, method = 'GET', payload = null, headers = {}) {
+  const options = { method };
+  if (payload) {
+    options.headers = { 'Content-Type': 'application/json', ...headers };
+    options.body = JSON.stringify(payload);
+  } else {
+    options.headers = headers;
+  }
+  const token = getToken();
+  if (token) {
+    options.headers.Authorization = `Bearer ${token}`;
+  }
+  const res = await fetch(url, options);
+  if (res.ok) return res.json();
+  throw new Error('Bad Request');
 }
-  
