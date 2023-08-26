d5f961e58a915f7d1b266abd5f5e21af12b4043a
mxrgxr <mgxrcias@gmail.com>
2023-08-26 04:03:26 -0700

    create playlists api module

--- src/utilities/playlists-api.js
+++ src/utilities/playlists-api.js
@@ -1,7 +1,6 @@
 import sendRequest from "./send-request";
 const BASE_URL = '/api/playlists';
 
-export async function getUserPlaylists() {
-    console.log('backend api get playlists triggered')
-    return sendRequest(BASE_URL);
+export async function getUserPlaylists(accessToken) {
+    return sendRequest(BASE_URL, 'GET', null, { Authorization: `Bearer ${accessToken}` });
 }
