a0a61f6a4f6269e7210ae07dc993bd7e93934686
mxrgxr <mgxrcias@gmail.com>
2023-08-26 14:27:28 -0700

    fetch user from stored token

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,10 +1,24 @@
 export function getToken() {
     const token = localStorage.getItem('token');
     if (!token) return null;
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    if (payload.exp * 1000 < Date.now()) {
-      localStorage.removeItem('token');
+    return token;
+}
+
+export async function getUser() {
+    const token = getToken();
+    if (!token) return null;
+  
+    try {
+      const response = await fetch('/api/users', {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+  
+      const data = await response.json();
+      return data.user;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
       return null;
     }
-    return token;
 }
